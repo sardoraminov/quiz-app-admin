544968d77a5bdd9c756b9ba099c7379210bfb318
sardoraminov <oddiydasturchi@gmail.com>
2022-05-20 21:18:16 +0500

    changes

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://quizapp-server.herokuapp.com/",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5000/"
+      : "https://quizapp-server.herokuapp.com/",
 });
