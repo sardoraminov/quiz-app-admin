dfae72d73e4dd3bccb6cb2e5b01456404c20f261
dasturchioka <dasturchioka@gmail.com>
2022-10-18 22:34:09 +0500

    not found page added

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import NotFound from "@/assets/not_found.jpg"
+</script>
+
+<template>
+    <div class="not-found">
+        <div class="img">
+            <img :src="NotFound" alt="not-found-page">
+        </div>
+        <div class="content">
+            <p>Bu sahifada hech qanday ma'lumot mavjud emas!</p>
+            <button @click="$router.go(-1)">Qaytish</button>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
